b7e4b771170349856a526163b22e72d1f4be36a4
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-01 22:07:54 +0300

    almost done

---
+++ app.js
@@ -0,0 +1,111 @@
+function userName() {
+
+    var names = prompt("please enter your name!")
+    return names;
+}
+
+function userGendre() {
+    var gendres = prompt("please enter your gendre as male or female!")
+    
+    return gendres;
+}
+
+
+function userAge() {
+    var ages = prompt("please enter your age!");
+    
+    return ages;
+}
+
+function checkAge() {
+    if(arr[2] <= 0){
+        var check = alert("age must be bigger than 0");
+        return check;
+    }
+}
+
+function skipWelcomming() {
+    var skip = confirm("Do you want to skip the welcomming message?!!");
+    
+    return skip;
+}
+
+
+function lastCheck() {
+    
+    if (arr[2] <= 0 && arr[4] == false) {
+        return alert("welcome");
+    } else {
+        if (arr[4] == false && arr[1] == "male"){
+            return alert("Welcome Mr. " + arr[0]);
+        } else {
+                 alert("Welcome Ms. " + arr[0]);
+            }
+            } 
+
+    }
+
+
+
+
+
+
+
+function gymQuestion() {
+    var gym = confirm("do you like gym?");
+    return gym;
+
+}
+
+function studyQuestion() {
+    var study = confirm("do you like study?");
+    return study;
+}
+
+
+function cyclingQuestion() {
+    var cycling = confirm("do you like cycling?")
+    return cycling;
+}
+
+function CheckEmpty(e) {
+    
+    if (e == ''){
+        e = "invalid";
+    }
+    return e;
+}
+
+var arr = [];
+arr[0] = userName();
+arr[1] = userGendre();
+arr[2] = userAge();
+arr[3] = checkAge();
+arr[4] = skipWelcomming();
+arr[5] = lastCheck();
+
+arr[6] = gymQuestion();
+arr[7] = studyQuestion();
+arr[8] = cyclingQuestion();
+
+//console.log(arr);
+
+//CheckEmpty(arr[0]);
+//CheckEmpty(arr[1]);
+//CheckEmpty(arr[2]);
+
+
+//  for (var i = 0; i <= 2; arr ++) {
+//     CheckEmpty(arr[i]);
+// }
+
+
+// for(var j = 0; j < arr.length; j ++) {
+//     console.log(arr[j]);
+// }
+
+
+
+
+
+
